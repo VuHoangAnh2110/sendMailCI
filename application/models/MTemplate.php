e52d1cfd59756f4cfe4c6ee8f92108aed58b6286
VuHoangAnh2110 <134081915+VuHoangAnh2110@users.noreply.github.com>
2024-12-05 15:10:14 +0700

    Create MTemplate.php

---
+++ application/models/MTemplate.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MTemplate extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    // Lấy danh sách tất cả các template
+    public function get_list() {
+        $this->db->select("*");
+        $this->db->from("template_mail");
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    // Lấy thông tin template theo ID
+    public function get_template($id) {
+        if (is_numeric($id) && $id > 0) {
+            $this->db->select("*");
+            $this->db->from("template_mail");
+            $this->db->where("id_template ", $id);
+            $query = $this->db->get();
+            return $query->result();
+        } else {
+            return false;
+        }
+    }
+
+    // Chèn template mới vào cơ sở dữ liệu
+    public function insert_template($data) {
+        $this->db->insert("template_mail", $data);
+        return $this->db->insert_id();
+    }
+
+}
