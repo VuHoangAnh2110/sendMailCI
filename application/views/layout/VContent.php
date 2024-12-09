60edcbf33fccf1897be70de1a3bc7549e2c60ba7
VuHoangAnh2110 <134081915+VuHoangAnh2110@users.noreply.github.com>
2024-12-09 16:03:44 +0700

    Update VContent.php

--- application/views/layout/VContent.php
+++ application/views/layout/VContent.php
@@ -11,9 +11,10 @@
                         <div class="mb-4 w-[90%]">
                             <label for="email_subject" class="block text-gray-700 font-semibold mb-2">
                                 Tiêu đề email:
+                                <span id="requiredStar1" class="requiredStar text-gray-500 align-middle hidden text-red-500"> *!</span>
                             </label>
                             <input type="text" id="email_subject" name="email_subject" 
-                            class="w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-300 p-3" 
+                            class="inputForm w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-300 p-3" 
                             placeholder="Nhập tiêu đề email">
                         </div>
 
@@ -21,27 +22,30 @@
                         <div class="mb-4 w-[90%]">
                             <label for="sender_name" class="block text-gray-700 font-semibold mb-2">
                                 Tên người gửi:
+                                <span id="requiredStar2" class="requiredStar text-gray-500 align-middle hidden text-red-500"> *!</span>
                             </label>
                             <input type="text" id="sender_name" name="sender_name" 
-                            class="w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-300 p-3" 
+                            class="inputForm w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-300 p-3" 
                             placeholder="Nhập tên người gửi">
                         </div>
                         
                         <div class="mb-4 w-[90%]">
                             <label for="email_content" class="block text-gray-700 font-semibold mb-2">
                                 Mẫu nội dung email:
+                                <span id="requiredStar3" class="requiredStar text-gray-500 align-middle hidden text-red-500"> *!</span>
                             </label>
                             <textarea id="email_content" name="email_content" rows="6" 
-                            class="w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-300 p-3" 
+                            class="inputForm w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-300 p-3" 
                             placeholder="Nhập nội dung email với các placeholder như <<name>>, <<date>>..."></textarea>
                         </div>
                         
                         <div class="mb-4 w-[90%]">
                             <label for="data_file" class="block text-gray-700 font-semibold">
                                 Tải file Excel dữ liệu:
+                                <span id="requiredStar4" class="requiredStar text-gray-500 align-middle hidden text-red-500"> *!</span>
                             </label>
                             <input type="file" id="data_file" name="data_file" accept=".xls,.xlsx" 
-                            class="text-gray-700 border w-full border-gray-300 rounded-md p-2">
+                            class="inputForm text-gray-700 border w-full border-gray-300 rounded-md p-2">
                         </div>
                         
                         <div class="mt-5 grid grid-cols-4 gap-5">
@@ -85,9 +89,9 @@
         </form>
 
         <!-- Ô xem trước nội dung sau khi lưu vào database -->
-        <div id="preview" class="mt-8 hidden bg-gray-100 p-4 rounded-md shadow-inner mb-10">
+        <div id="preview" class="sm:mx-20 sm:w-2/3 p-1 mt-8 hidden bg-gray-100 p-4 rounded-md shadow-inner mb-10">
             <h2 class="text-xl font-bold mb-2">Xem trước nội dung email</h2>
-            <div id="preview_content">
+            <div id="preview_content" class="mt-1">
             </div>
         </div>
     </div>
